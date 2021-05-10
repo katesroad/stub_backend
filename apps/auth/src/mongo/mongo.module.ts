c44e86831e1e7a0323d5b48f134c9978b54cb9da
Kate <test@gmail.com>
2021-05-10 15:35:35 -0400

    use MongoDB from kubernetes

--- apps/auth/src/mongo/mongo.module.ts
+++ apps/auth/src/mongo/mongo.module.ts
@@ -4,7 +4,7 @@ import { User, UserSchema } from './user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/users'),
+    MongooseModule.forRoot('mongodb://auth-mongo-srv:27017/auth'),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   exports: [
